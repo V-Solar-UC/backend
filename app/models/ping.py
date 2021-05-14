58db74c7497f4319bf46933194da6ba4c45b0d01
rieg-ec <rieg@uc.cl>
2021-05-14 02:12:48 -0400

    refactor(models): changed pydantic to schemas/
    sqlalchemy models should use "model" keyword
    and pydantic will stay in schemas/

--- app/models/ping.py
+++
@@ -1,7 +0,0 @@
-from enum import Enum
-from typing import Optional
-
-from app.models.core import CoreModel
-from app.models.core import IDModelMixin
-
-# TODO: create data models inheriting CoreModel and IDModelMixin
