2ab1134cc07f5f41e356f1ec657d0d82313f9994
rieg-ec <rieg@uc.cl>
2021-05-25 16:43:03 -0400

    refactor(database): remove encode/databases & use sqlalchemy async
    removed encode/databases as the database library and psycopg2
    as sync driver and instead moved fully to sqlalchemy and asyncpg
    which supports async queries since v1.4. Also removed repositories
    layer and will later use a more common service layer to perform db queries.

--- app/core/config.py
+++ app/core/config.py
@@ -1,6 +1,5 @@
 import logging
 
-from databases import DatabaseURL
 from starlette.config import Config
 from starlette.datastructures import Secret
 
@@ -15,11 +14,7 @@ POSTGRES_SERVER: str = config('POSTGRES_SERVER', cast=str, default='db')
 POSTGRES_PORT: str = config('POSTGRES_PORT', cast=str, default='5432')
 POSTGRES_DB: str = config('POSTGRES_DB', cast=str)
 
-DATABASE_URL = config(
-    'DATABASE_URL',
-    cast=DatabaseURL,
-    default=f'postgresql://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
-)
+DATABASE_URL = f'postgresql+asyncpg://{POSTGRES_USER}:{POSTGRES_PASSWORD}@{POSTGRES_SERVER}:{POSTGRES_PORT}/{POSTGRES_DB}'
 
 DEBUG: bool = config('DEBUG', cast=bool, default=False)
 
--- app/core/tasks.py
+++ app/core/tasks.py
@@ -2,17 +2,16 @@ from typing import Callable
 
 from fastapi import FastAPI
 
-from app.db.tasks import close_db_connection
-from app.db.tasks import connect_to_db
+from app.db.tasks import create_engine
 
 
 def create_start_app_handler(app: FastAPI) -> Callable:
     async def start_app() -> None:
-        await connect_to_db(app)
+        await create_engine(app)
     return start_app
 
 
 def create_stop_app_handler(app: FastAPI) -> Callable:
     async def stop_app() -> None:
-        await close_db_connection(app)
+        pass
     return stop_app
