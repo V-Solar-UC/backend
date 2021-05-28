2a6770db24a8ccacbc8435abef08f29071a9259d
rieg-ec <rieg@uc.cl>
2021-05-27 23:33:09 -0400

    feat(tests): add donor tests
    pytest pixtures and basic crud-like tests
    for donor route

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,42 +1,33 @@
 import os
-import warnings
 
 import pytest
 from alembic import command
 from alembic.config import Config
 from asgi_lifespan import LifespanManager
-from databases import Database
 from fastapi import FastAPI
 from httpx import AsyncClient
 
 from app.main import get_app
 
 
-@pytest.fixture(scope='session')
+@pytest.fixture(scope='session', autouse=True)
 def apply_migrations():
-    # warnings.filterwarnings("ignore", category=DeprecationWarning)
     os.environ['TESTING'] = '1'
     config = Config('alembic.ini')
 
     command.upgrade(config, 'head')
     yield
-    # TODO: do i need to downgrade if using force_rollback in db object?
     command.downgrade(config, 'base')
     os.environ['TESTING'] = ''
 
 
-@pytest.fixture
-def app(apply_migrations: None) -> FastAPI:
+@pytest.fixture(scope='session')
+def app() -> FastAPI:
     app = get_app()
     return app
 
 
-@pytest.fixture
-def db(app: FastAPI) -> Database:
-    return app.state.db
-
-
-@pytest.fixture
+@pytest.fixture()
 async def client(app: FastAPI) -> AsyncClient:
     async with LifespanManager(app):
         async with AsyncClient(
