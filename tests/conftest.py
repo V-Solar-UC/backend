2ced3b8d7e1e45f114885200e14d4833a9df594b
Benjamín Lepe <48299074+BenjaLepe@users.noreply.github.com>
2021-09-15 19:52:53 -0300

    :white_check_mark: User: add create and login test

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,9 +5,12 @@ from alembic import command
 from alembic.config import Config
 from asgi_lifespan import LifespanManager
 from fastapi import FastAPI
+from fastapi.exceptions import HTTPException
 from httpx import AsyncClient
 
+from app.api.services.user import UserService
 from app.main import get_app
+from app.schemas.user import UserInDB
 
 
 @pytest.fixture(scope='session', autouse=True)
@@ -21,18 +24,40 @@ def apply_migrations():
     os.environ['TESTING'] = ''
 
 
-@pytest.fixture(scope='session')
-def app() -> FastAPI:
+@pytest.fixture()
+async def app() -> FastAPI:
     app = get_app()
-    return app
+    async with LifespanManager(app):
+        yield app
+
+
+@pytest.fixture()
+async def user(app: FastAPI):
+    async with app.state.async_session() as session:
+        user_data = {
+            'username': 'test',
+            'name': 'test name',
+            'career': 'test career',
+            'team': 'team test',
+            'profile_photo_path': '',
+            'email': 'test@mail.com',
+            'password': 'testpws',
+        }
+        try:
+            user = await UserService.find_by(session, 'email', user_data['email'])
+        except HTTPException:
+            hashed_password = UserService.password_hash(password=user_data['password'])
+            user_data.update({'hashed_password': hashed_password})
+            user = await UserService.create(session, UserInDB(**user_data))
+        finally:
+            return user
 
 
 @pytest.fixture()
 async def client(app: FastAPI) -> AsyncClient:
-    async with LifespanManager(app):
-        async with AsyncClient(
-            app=app,
-            base_url='http://testserver',
-            headers={'Content-Type': 'application/json'}
-        ) as client:
-            yield client
+    async with AsyncClient(
+        app=app,
+        base_url='http://testserver',
+        headers={'Content-Type': 'application/json'}
+    ) as client:
+        yield client
